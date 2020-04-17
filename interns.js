be7629fcb49048d5a32792b8c37a257687ea0f13
Mamus Eferha <eferhamamus@gmail.com>
2020-04-17 13:01:18 +0100

    crud-basics

--- interns.js
+++ interns.js
@@ -1,33 +1,21 @@
-const assert = require('assert'); 
+const assert = require('assert');
 
-const insertDocuments = function(db, callback) {
-
-    // Get the documents collection
-    
+const createInterns = function(db, callback) {
     const collection = db.collection('myMovies');
-    
-    // Insert some documents
-    
     collection.insertMany([
-    
         {movie: "The Banker", year: "2020", rating: "8"},
         {movie: "Bad Boys", year: "2020", rating: "7"},
         {movie: "The Hunt", year: "2020", rating: "7"},
         {movie: "Bloodshot", year: "2020", rating: "7.5"},
         {movie: "First Cow", year: "2020", rating: "6.5"},
-    
     ], function(err, result) {
-    
-    assert.equal(err, null);
-    
-    assert.equal(3, result.result.n);
-    
-    assert.equal(3, result.ops.length);
-    
-    console.log("Inserted 3 documents into the collection");
-    
-    callback(result);
-    
+        assert.equal(err, null);
+        assert.equal(5, result.result.n);
+        assert.equal(5, result.ops.length);
+        console.log("Inserted 5 documents successfully");
+        console.log(result);
+        callback(result);
     });
-    
-    };
+};
+
+module.exports = createInterns;
--- mongodb.js
+++ mongodb.js
@@ -1,13 +1,23 @@
-var MongoClient = require('mongodb').MongoClient;
+var mongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const createInterns = require('./interns');
+const findInterns = require('./findinterns');
+const updateInterns = require('./updateinterns');
 
-var url = "mongodb://localhost:27017/debbsefe";
+var url = 'mongodb://localhost:27017/mydb';
 
-MongoClient.connect(url, function(err, db) {
+const dbname = 'debbsefe';
+const client = new mongoClient(url);
 
-if (err) throw err;
-
-console.log("Database created by debbsefe!");
-
-db.close();
-
+client.connect(function(err) {
+    assert.equal(null, err);
+    console.log('Database created by debbsefe!');
+    
+            const db = client.db(dbname);
+                createInterns(db, function(){
+                findInterns(db, function() {
+                updateInterns(db, function(){   
+            });                 
+        });
+    });
 });
